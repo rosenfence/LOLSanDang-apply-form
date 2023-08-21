c47bc2ed0f802beeba9a32fa5c407feadac84084
rosenfence <rosenfence@gmail.com>
2023-08-21 22:21:13 +0900

    setup mongoDB connection

---
+++ util/database.ts
@@ -0,0 +1,13 @@
+import { MongoClient } from 'mongodb';
+const url = process.env.NEXT_PUBLIC_MONGODB_URL as string;
+let connectDB: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url).connect();
+}
+export { connectDB };
