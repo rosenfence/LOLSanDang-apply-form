d600f532dbf9a1307babf7dac6a60ccf77107daa
rosenfence <rosenfence@gmail.com>
2023-08-23 01:57:55 +0900

    chore: move buttons to PublishForm component

--- app/publish/page.tsx
+++ app/publish/page.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import PublishForm from './PublishForm';
-import Link from 'next/link';
 
 const Publish = () => {
   return (
     <form className='flex-center flex-col'>
       <div>롤할 사람을 모집합시다</div>
       <PublishForm />
-      <button className='button'>작성하기</button>
-      <Link href='/list'>
-        <button className='button'>돌아가기</button>
-      </Link>
     </form>
   );
 };
