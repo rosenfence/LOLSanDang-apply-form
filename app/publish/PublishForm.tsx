2f077c2284d5b1691a6350cca91e9ea31ff7ca26
rosenfence <rosenfence@gmail.com>
2023-08-25 12:11:59 +0900

    feat: client - publish form page

--- app/publish/PublishForm.tsx
+++ app/publish/PublishForm.tsx
@@ -1,5 +1,10 @@
 'use client';
 import React, { useState } from 'react';
+import { PositionType } from '@/interface/interface';
+import Link from 'next/link';
+import { useMutation } from '@tanstack/react-query';
+import { postLists } from '@/function/postLists';
+import { useRouter } from 'next/navigation';
 
 const PublishForm = () => {
   const [title, setTitle] = useState<string>('');
@@ -10,8 +15,11 @@ const PublishForm = () => {
 
   const [nickname, setNickname] = useState<string>('');
   const [tier, setTier] = useState<string>('C');
-  const [position, setPosition] = useState<string[]>([]);
+  const [position, setPosition] = useState<PositionType>();
 
+  const router = useRouter();
+
+  // title,content, date, time, nickname EventListener
   const handleChangeForInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target.id;
     switch (target) {
@@ -33,12 +41,94 @@ const PublishForm = () => {
     }
   };
 
+  // type EventListener
   const handleClickForInput = (e: React.MouseEvent<HTMLInputElement>) => {
     setType((e.target as HTMLInputElement).value);
   };
 
+  // tier EventListener
   const handleChangeForSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTier(e.target.value);
+  };
+
+  // initial position object
+  const positionObj: PositionType = {
+    TOP: false,
+    JUG: false,
+    MID: false,
+    ADC: false,
+    SUP: false,
+  };
+
+  // object changer
+  const handleClickToArray = (e: React.MouseEvent<HTMLInputElement>) => {
+    const pos = (e.target as HTMLInputElement).id;
+    positionObj[pos] = !positionObj[pos];
+  };
+
+  // position vaildation function
+  const isValidPosition = (obj: PositionType) => {
+    let arr = Object.values(obj);
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === false) {
+        counter++;
+      }
+    }
+    if (counter === 5) {
+      return false;
+    }
+    return true;
+  };
+
+  // request body
+  const reqBody = {
+    title,
+    content,
+    date,
+    time,
+    gameType: type,
+    member: [{ name: nickname, tier, position }],
+  };
+
+  // react-query mutation
+  const { mutate } = useMutation({
+    mutationFn: () => postLists(reqBody),
+    onSuccess: () => {
+      console.log('데이터 추가 성공');
+    },
+  });
+
+  // submit validater
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (title === '') {
+      alert('모집 제목을 입력해주세요.');
+      return;
+    } else if (date === '') {
+      alert('모일 날짜를 입력해주세요.');
+      return;
+    } else if (time === '') {
+      alert('게임할 시간을 입력해주세요.');
+      return;
+    } else if (type === '') {
+      alert('게임 타입을 선택해주세요.');
+      return;
+    } else if (nickname === '') {
+      alert('게임 닉네임을 입력해주세요.');
+      return;
+    } else if (type === 'SR') {
+      if (!isValidPosition(positionObj)) {
+        alert('희망 포지션을 1가지 이상 선택해주세요.');
+        return;
+      }
+    }
+
+    setPosition(positionObj);
+
+    mutate();
+    alert('정상적으로 등록되었습니다');
+    router.push('/list');
   };
 
   return (
@@ -67,7 +157,7 @@ const PublishForm = () => {
       </div>
       <div className='form-div'>
         <label htmlFor='time'>시간</label>
-        <input id='time' type='time' />
+        <input onChange={(e) => handleChangeForInput(e)} id='time' type='time' />
       </div>
       <div className='form-div'>
         <p>게임 타입</p>
@@ -105,7 +195,12 @@ const PublishForm = () => {
       <div className='form-div'>
         <span>
           <label htmlFor='nickname'>닉네임</label>
-          <input type='text' id='nickname' placeholder='닉네임' />
+          <input
+            onChange={(e) => handleChangeForInput(e)}
+            type='text'
+            id='nickname'
+            placeholder='닉네임'
+          />
         </span>
         <span>
           <label htmlFor='tier'>티어</label>
@@ -126,27 +221,38 @@ const PublishForm = () => {
         <span>
           <h3>포지션</h3>
           <span>
-            <input type='checkbox' id='TOP' name='TOP' />
+            <input onClick={(e) => handleClickToArray(e)} type='checkbox' id='TOP' name='TOP' />
             <label htmlFor='TOP'>탑</label>
           </span>
           <span>
-            <input type='checkbox' id='JUG' name='JUG' />
+            <input onClick={(e) => handleClickToArray(e)} type='checkbox' id='JUG' name='JUG' />
             <label htmlFor='JUG'>정글</label>
           </span>
           <span>
-            <input type='checkbox' id='MID' name='MID' />
+            <input onClick={(e) => handleClickToArray(e)} type='checkbox' id='MID' name='MID' />
             <label htmlFor='MID'>미드</label>
           </span>
           <span>
-            <input type='checkbox' id='ADC' name='ADC' />
+            <input onClick={(e) => handleClickToArray(e)} type='checkbox' id='ADC' name='ADC' />
             <label htmlFor='ADC'>원딜</label>
           </span>
           <span>
-            <input type='checkbox' id='SUP' name='SUP' />
+            <input onClick={(e) => handleClickToArray(e)} type='checkbox' id='SUP' name='SUP' />
             <label htmlFor='SUP'>서포터</label>
           </span>
         </span>
       </div>
+      <button
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+        className='button'
+      >
+        작성하기
+      </button>
+      <Link href='/list'>
+        <button className='button'>돌아가기</button>
+      </Link>
     </>
   );
 };
