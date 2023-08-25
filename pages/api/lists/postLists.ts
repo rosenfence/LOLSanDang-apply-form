7a0be40cd46dffaaffd2efab8eab2b2624310a87
rosenfence <rosenfence@gmail.com>
2023-08-25 12:11:38 +0900

    feat: server - post List

--- pages/api/lists/postLists.ts
+++ pages/api/lists/postLists.ts
@@ -3,8 +3,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function getLists(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
+    let query = JSON.parse(req.body);
+    query.isEnded = false;
     let db = (await connectDB).db('apply');
-    let lists = await db.collection('list').insertOne(req.body);
+    let lists = await db.collection('list').insertOne(query);
     res.status(200).json('목록 작성 완료');
   } else {
     res.status(400).json('bad request');
